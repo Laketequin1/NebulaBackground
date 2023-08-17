1de1a4f505d9645f17525a053077a36190f0c946
Laketequin1 <laketequin1@gmail.com>
2023-08-17 16:30:04 +1200

    Script update

--- test/scripts.js
+++ test/scripts.js
@@ -52,7 +52,7 @@ var rand = function (rMi, rMa) {
     return ~~(Math.random() * (rMa - rMi + 1)) + rMi;
 };
 
-ctx.lineCap = "round";
+ctx.lineCap = "square";
 var stars = [];
 var shootingStars = [];
 
@@ -92,13 +92,7 @@ function createStar(mx, my) {
         alpha: 1 - Math.abs(dist) / cw,
         draw: function () {
             ctx.fillStyle = "rgba(255, 255, 255, 1)";
-            ctx.fillRect(Math.floor(this.x - this.size / 2), Math.floor(this.y - this.size / 2), this.size, this.size);
-            //ctx.lineWidth = this.size;
-            //ctx.beginPath();
-            //ctx.moveTo(this.lastX, this.lastY);
-            //ctx.lineTo(this.x, this.y);
-            //ctx.stroke();
-        },
+            ctx.fillRect(Math.floor(this.x - this.size / 2), Math.floor(this.y - this.size / 2), this.size, this.size);        },
         update: function () {
             this.lastX = this.x;
             this.lastY = this.y;
@@ -108,27 +102,28 @@ function createStar(mx, my) {
     });
 }
 
-function createShootingStar(mx, my) {
+function createShootingStar(mx, my, starAngle = Math.random() * Math.PI * 2, starOpacity = 1, starSize = null) {
     var dx = cw / 2 - mx;
     var dy = ch / 2 - my;
     var dist = Math.sqrt(dx * dx + dy * dy);
     var sizeRandom = Math.random();
-    var starSize;
-    if (sizeRandom < 0.55) {
-        starSize = 1; // 50% chance for size 1
-    } else if (sizeRandom < 0.9) {
-        starSize = 2; // 40% chance for size 2
-    } else {
-        starSize = 3; // 10% chance for size 3
+    if (starSize == null) {
+        if (sizeRandom < 0.55) {
+            starSize = 15; // 50% chance for size 1
+        } else if (sizeRandom < 0.9) {
+            starSize = 16; // 40% chance for size 2
+        } else {
+            starSize = 14; // 10% chance for size 3
+        }
     }
-    stars.push({
+    shootingStars.push({
         x: mx,
         y: my,
         lastX: mx,
         lastY: my,
         hue: 0,
-        colorAngle: mx / 15 + 250,
-        angle: Math.PI,
+        opacity: starOpacity,
+        angle: starAngle,
         size: starSize,
         centerX: cw / 2,
         centerY: ch / 2,
@@ -136,26 +131,21 @@ function createShootingStar(mx, my) {
         speed: (starSize / 14) * (dist / 1100) + 0.04,
         alpha: 1 - Math.abs(dist) / cw,
         draw: function () {
-            var starGradient = ctx.createLinearGradient(0, 0, 0, ch);
-            starGradient.addColorStop(clamp(this.y / ch, 0, 1), "rgba(255, 255, 255, 1)");
-            //  starGradient.addColorStop(clamp((this.y + this.size-1) / ch, 0, 1), "rgba(255, 255, 255, 1)");
-            //  starGradient.addColorStop(clamp((this.y + this.size) / ch, 0, 1), "rgba(255, 255, 255, 0.2)");
-            //  starGradient.addColorStop(clamp((this.y + this.size * trailSize) / ch, 0, 1), "rgba(255, 255, 255, 0)");
-            ctx.fillStyle = "rgba(255, 255, 255, 1)";
-            ctx.strokeStyle = "hsla(" + this.colorAngle + ",100%,50%,1)";
+            ctx.fillStyle = "hsla(" + (this.x + Math.sqrt(this.x * this.y)) + ", 100%, 50%, " + this.opacity + ")";
             ctx.fillRect(Math.floor(this.x - this.size / 2), Math.floor(this.y - this.size / 2), this.size, this.size);
-            //ctx.lineWidth = this.size;
-            //ctx.beginPath();
-            //ctx.moveTo(this.lastX, this.lastY);
-            //ctx.lineTo(this.x, this.y);
-            //ctx.stroke();
         },
-        update: function () {
+        update: function() {
             this.lastX = this.x;
             this.lastY = this.y;
-            this.x = this.x;
-            this.y = this.y - 2 * this.speed;
-        },
+            
+            // Calculate new x and y positions based on angle and speed
+            //this.x = this.x + Math.cos(this.angle) * this.radius;
+            //this.y = this.y + Math.sin(this.angle) * this.radius;
+        
+            // Move along the angle by subtracting 2 * this.speed
+            this.x -= 2 * this.speed * Math.cos(this.angle);
+            this.y -= 2 * this.speed * Math.sin(this.angle);
+        },        
     });
 }
 
@@ -163,42 +153,9 @@ function clear() {
     stars = [];
 }
 
-function set_opacity(canvasC, canvasCtx, opacity) {
-    origionalGlobalAlpha = canvasCtx.globalAlpha;
-
-    transparentTrailCtx.clearRect(0, 0, cw, ch);
-    transparentTrailCtx.drawImage(canvasC, 0, 0);
-
-    canvasCtx.clearRect(0, 0, cw, ch);
-    canvasCtx.globalAlpha = opacity;
-    canvasCtx.globalCompositeOperation = "destination-over";
-    canvasCtx.drawImage(transparentTrailC, 0, 0);
-    canvasCtx.globalAlpha = origionalGlobalAlpha;
-}
-
-function remove_min_opacity(canvasC, canvasCtx) {
-    transparentTrailCtx.clearRect(0, 0, cw, ch);
-    transparentTrailCtx.drawImage(canvasC, 0, 0);
-
-    canvasCtx.clearRect(0, 0, cw, ch);
-    ctx.globalCompositeOperation = "destination-out";
-    canvasCtx.drawImage(transparentTrailC, 0, 0);
-    ctx.globalCompositeOperation = "source-over";
-}
-
 var loop = function () {
     window.requestAnimFrame(loop);
     if (trail) {
-        // Set the gradient as the fill style
-        //ctx.fillStyle = gradient;
-
-        //ctx.drawImage(trailC, 0, 0);
-        //set_opacity(c, ctx, 0.45);
-        //remove_min_opacity(c, ctx);
-
-        //trailCtx.clearRect(0, 0, cw, ch);
-        //ctx.clearRect(0, 0, cw, ch);
-        //ctx.drawImage(trailC, 0, 0);
         ctx.clearRect(0, 0, cw, ch);
     } else {
         ctx.clearRect(0, 0, cw, ch);
@@ -214,6 +171,18 @@ var loop = function () {
         if (star.y < -20) {
             stars.splice(i, 1);
         }
+    }
+    var i = shootingStars.length;
+    while (i--) {
+        var shootingStar = shootingStars[i];
+        var updateCount = 3;
+        while (updateCount--) {
+            shootingStar.update();
+            shootingStar.draw(ctx);
+        }
+        //if (star.y < -20) {
+        //    stars.splice(i, 1);
+        //}
     }
 };
 
